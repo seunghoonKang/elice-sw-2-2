05b9b90aafc9ee078b07cf8231656c772f4320fc
seunghoonKang <tmdgns4321@gmail.com>
2022-04-18 11:13:13 +0900

    자바스크립트분리

---
+++ 0418/daynight.js
@@ -0,0 +1,23 @@
+function night() {
+  document.querySelector('body').style.backgroundColor = 'black';
+
+  document.querySelector('body').style.color = 'white';
+  let as = document.querySelectorAll('a');
+  for (let i = 0; i < as.length; i = i + 1) {
+      as[i].style.color = 'white';
+  }
+  // $('body').css('background-color', 'black');
+  // $('body').css('color', 'white');
+  // $('a').css('color', 'white').css('text-decoration', 'underline');
+}
+
+function day() {
+  // 아니라면 아래 코드가 실행된다. 
+  document.querySelector('body').style.backgroundColor = 'white';
+  document.querySelector('body').style.color = 'black';
+  // 이 버튼의 value값을 night로 변경한다. 
+  let as = document.querySelectorAll('a');
+  for (let i = 0; i < as.length; i = i + 1) {
+      as[i].style.color = 'black';
+  }
+}
